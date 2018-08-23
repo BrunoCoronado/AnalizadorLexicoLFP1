da030e5300956ab783ae7e92eb488b8fc9362439
BrunoCoronado <bcoronado.morales@gmail.com>
2018-08-23 17:21:55 -0600

    Agregando elementos a la vista y funcionalidad basica

--- AnalizadorLexicoLFP1/MainWindow.xaml.vb
+++ AnalizadorLexicoLFP1/MainWindow.xaml.vb
@@ -1,3 +1,27 @@
 ﻿Class MainWindow
 
+    'StringCollection GetLinesCollectionFromTextBox(TextBox textBox)
+
+    'StringCollection lines = New StringCollection();
+
+    '// lineCount may be -1 if TextBox layout info Is Not up-to-date.
+    'int lineCount = TextBox.LineCount;
+
+    'For (int line = 0; line < lineCount; line++)
+    '    // GetLineText takes a zero-based line index.
+    '    lines.Add(textBox.GetLineText(line));
+
+    'Return lines;
+    '}
+
+    Private Sub analizar(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Dim noLineas = txtContenido.LineCount
+        Dim lineasContenido As New ArrayList
+
+        For i As Integer = 0 To (noLineas - 1)
+            lineasContenido.Add(txtContenido.GetLineText(i))
+        Next
+        'Dim ManejoDeDatos As New ManejoDeDatos(lineasContenido)
+    End Sub
+
 End Class
